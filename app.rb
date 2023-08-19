05d64dfaa227c7a1a28f051b4fc3d779708653bc
jelvas123 <137334054+jelvas123@users.noreply.github.com>
2023-08-19 22:39:13 +0000

    second

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ get("/square/new") do
     erb(:new_square_calc)
 end
 
-get'/square/results'do
+get("/square/results")do
   erb(:square_results)
 end
 
@@ -24,3 +24,25 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+<form action="/random/results">
+  <div>
+    <label for="min_input">
+      Minimum
+    </label>
+
+    <input id="min_input" type="text" name="user_min" placeholder="E.g. 1.5">
+  </div>
+
+  <div>
+    <label for="max_input">
+      Maximum
+    </label>
+
+    <input id="max_input" type="text" name="user_max" placeholder="E.g. 4.5">
+  </div>
+
+  <button>
+    Pick random number
+  </button>
+</form>
