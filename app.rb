4a0a026e46ed3268553e67b8ce6afad504c0217b
jelvas123 <137334054+jelvas123@users.noreply.github.com>
2023-08-19 22:33:02 +0000

    first

--- app.rb
+++ app.rb
@@ -1,6 +1,23 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get ("/howdy") do
+    erb(:hello)
+end
+
+get("/goodbye)do
+  erb(:bye)
+
+end 
+
+get("/square/new") do
+    erb(:new_square_calc)
+end
+
+get'/square/results'do
+  erb(:square_results)
+end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
